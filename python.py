a5e55b56046e4c047222220d8ff3de56a3616e55
Fruedee <88207468+Fruedeeg@users.noreply.github.com>
2023-05-11 13:34:11 +0200

    ff

--- python.py
+++ python.py
@@ -6,6 +6,7 @@ win = pygame.display.set_mode((1000, 1000))
 z = 0
 x = 0
 v = 0
+
 xplayer = 200
 yplayer = 600
 xobject = [30]
@@ -15,22 +16,24 @@ height = 20
 vel = 2
 img = pygame.image.load('img.jpg')
 run = True
-
+rect1 = pygame.Rect((xobject[0], yobject[0], width, height))
+rect2 = pygame.Rect((xplayer, yplayer, width, height))
 def object(xnumber, ynumber):
     pygame.draw.rect(win, (255, 0, 0), (xobject[xnumber], yobject[ynumber], width, height))
     xobject.append(random.randint(0,500))
     yobject.append(random.randint(0,1000))
-    
+
 def randomobject(g):
     object(z+g, x+g)
-    yobject[g] += 1
+    yobject[g] += 2
 
-def fail(g):
-    if xobject[g] == xplayer+5 or xobject[g] == xplayer-5 and yobject[g] == yplayer+5 or yobject[g] == yplayer-5:
+def fail(f):
+    if yobject[f] == yplayer:
         win.blit(img,(200,300))
         pygame.display.flip()
         pygame.display.update()
         pygame.time.wait(2000)
+
 while run:
     pygame.time.delay(10)
     for event in pygame.event.get():
@@ -45,26 +48,22 @@ while run:
 #        y -= vel
 #    if keys[pygame.K_DOWN] and y<500-height:
 #        y += vel
-
-    win.fill((0, 0, 0))
+    win.fill(0)
+    pygame.draw.rect(win, (0, 255, 0), (xplayer, yplayer, width, height)) 
     randomobject(0)
-    randomobject(1)
-    randomobject(2)
-    randomobject(3)
-    randomobject(4)
-    randomobject(5)
-    randomobject(6)
+    #randomobject(1)
+    #randomobject(2)
+    #randomobject(3)
+    #randomobject(4)
+    #randomobject(5)
+    #randomobject(6)
     fail(0)
-    fail(1)
-    fail(2)
-    fail(3)
-    fail(4)
-    fail(5)
-    fail(6)
-
-
-
-    pygame.draw.rect(win, (0, 255, 0), (xplayer, yplayer, width, height))      
+    #fail(1)
+    #fail(2)
+    #fail(3)
+    #fail(4)
+    #fail(5)
+    #fail(6)
     pygame.display.update()
     
 pygame.quit()
